3ac2c01175d633f4e6a22c5cdc3a1d58812318ad
sorianog <13261711+sorianog@users.noreply.github.com>
2025-05-07 15:49:55 -0700

    Setup flow for handling api response

--- app/src/main/java/com/sorianog/newsshortsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/sorianog/newsshortsapp/repository/NewsRepository.kt
@@ -2,6 +2,10 @@ package com.sorianog.newsshortsapp.repository
 
 import com.sorianog.newsshortsapp.data.datasource.NewsDataSource
 import com.sorianog.newsshortsapp.data.entity.TopHeadlinesResponse
+import com.sorianog.utilities.ResourceState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -9,7 +13,19 @@ class NewsRepository @Inject constructor(
     private val newsDataSource: NewsDataSource
 ) {
 
-    suspend fun getTopNewsHeadlines(country: String): Response<TopHeadlinesResponse> {
-        return newsDataSource.getTopNewsHeadlines(country)
+    suspend fun getTopNewsHeadlines(country: String): Flow<ResourceState<TopHeadlinesResponse>> {
+        return flow {
+            emit(ResourceState.Loading())
+
+            val response = newsDataSource.getTopNewsHeadlines(country)
+
+            if (response.isSuccessful && response.body() != null) {
+               emit(ResourceState.Success(response.body()!!))
+            } else {
+                emit(ResourceState.Error("Error fetching top news headlines"))
+            }
+        }.catch { e ->
+            emit(ResourceState.Error(e.localizedMessage ?: "Error in flow occurred"))
+        }
     }
 }
